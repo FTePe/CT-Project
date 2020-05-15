c843904d794ca40f652b6350e4a000798d710a00
Francis Treherne Pollock <francistp@live.com>
2020-05-15 12:35:57 +0100

    Calibrate working

--- ct_calibrate.py
+++ ct_calibrate.py
@@ -17,52 +17,41 @@ def ct_calibrate(photons, material, sinogram, scale, correct=True):
 
 	# Get dimensions and work out detection for just air of twice the side
 	# length (has to be the same as in ct_scan.py)
-	# n = sinogram.shape[1]
-	# length = 2*n*scale
-	# energies=len(photons)
-	# samples = length
-	# detector_photons = np.zeros([energies, samples])
-	# for e in range(energies):
-	# 	detector_photons[e] = photons[e]
-	# attenuate(detector_photons, material.coeff('Air'), np.arange(0,length, length/scale) )
+	angles = sinogram.shape[0]
+	n = sinogram.shape[1]
+	xi, yi = np.meshgrid(np.arange(n) - (n/2) + 0.5, np.arange(n) - (n/2) + 0.5)
 
 
+	scan = np.zeros((angles, n))
+	for angle in range(angles):
 
+		sys.stdout.write("Calibrating angle: %d   \r" % (angle + 1) )
 
-	# scan = np.zeros((angles, n))
-	# for angle in range(angles):
+		# Get rotated coordinates for interpolation
+		p = -math.pi / 2 - angle * math.pi / angles
+		x0 = xi * math.cos(p) - yi * math.sin(p) + (n/2) - 0.5
+		y0 = xi * math.sin(p) + yi * math.cos(p) + (n/2) - 0.5
 
-	# 	sys.stdout.write("Scanning angle: %d   \r" % (angle + 1) )
+		# For each material, add up how many pixels contain this on each ray
+		depth = np.zeros((len(material.coeffs), n))
 
-	# 	# Get rotated coordinates for interpolation
-	# 	p = -math.pi / 2 - angle * math.pi / angles
-	# 	x0 = xi * math.cos(p) - yi * math.sin(p) + (n/2) - 0.5
-	# 	y0 = xi * math.sin(p) + yi * math.cos(p) + (n/2) - 0.5
+		interpolated = scipy.ndimage.map_coordinates(np.ones((n,n)), [y0, x0], order=1, mode='constant', cval=0, prefilter=False)
+		
+		# only necessary for more complex forms of interpolation above
+		# depth = np.clip(depth, 0, None)
 
-	# 	# For each material, add up how many pixels contain this on each ray
-	# 	depth = np.zeros((len(material.coeffs), n))
+		# ensure an appropriate amount of air is included in the calculation
+		# to account for the scan being circular, but the phantom being square
+		# diameter of circle taken to be twice the phantom side length
+		depth = 2 * n - np.sum(interpolated, axis=0)
+		# scale the depth appropriately and calculate detections for this set of
+		# materials
+		depth*= scale
+		print(depth)
 
-	# 	for index, m in enumerate(materials):
-	# 		interpolated = scipy.ndimage.map_coordinates(material_phantom[index], [y0, x0], order=1, mode='constant', cval=0, prefilter=False)
-	# 		depth[m] = np.sum(interpolated, axis=0)
-
-	# 	# only necessary for more complex forms of interpolation above
-	# 	depth = np.clip(depth, 0, None)
-
-	# 	# ensure an appropriate amount of air is included in the calculation
-	# 	# to account for the scan being circular, but the phantom being square
-	# 	# diameter of circle taken to be twice the phantom side length
-	# 	depth[air] = 2 * n - np.sum(depth, axis=0)
-
-	# 	# scale the depth appropriately and calculate detections for this set of
-	# 	# materials
-	# 	depth*= scale
-	
-	# 	scan[angle] = ct_detect(photons, material.coeff('Air'), depth) #, mas)
-
-	# use attenuate of 2*n*scale of air
+		scan[angle] = ct_detect(photons, material.coeff('Air'), depth, 1)# - this is correct 
 	
 	# perform calibration
-	# sinogram = -np.log( np.divide(sinogram, scan))
+	sinogram = -np.log( np.divide(scan, sinogram))
 
 	return sinogram
