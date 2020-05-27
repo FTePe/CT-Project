b587c4312debb4e5fe4465926b92401453cd2608
Sanjan Das <33346052+Sanjan611@users.noreply.github.com>
2020-05-27 20:38:55 +0100

    edited as per Treece's advice. Should be fully alright now

--- hu.py
+++ hu.py
@@ -12,27 +12,35 @@ def hu(p, material, reconstruction, scale):
 	Units, using the material coefficients, photon energy p and scale given."""
 	
 	n = reconstruction.shape[0]
-	angles = 180
+	#angles = 180
 
 	# use water to calibrate
 	# calibration done by using a n x n phantom made of water
-	phantom_water = ct_phantom(material.name, n, 10)
+	#phantom_water = ct_phantom(material.name, n, 10)
 
 	# put this through the same calibration process as the normal CT data
-	sinogram = ct_scan(p, material, phantom_water, scale, angles)
-	sinogram_att = ct_calibrate(p, material, sinogram, scale)
-	filtered = ramp_filter(sinogram_att, scale)
-	reconstruction_water = back_project(filtered)
+	#sinogram = ct_scan(p, material, phantom_water, scale, angles)
+	#sinogram_att = ct_calibrate(p, material, sinogram, scale)
+	#filtered = ramp_filter(sinogram_att, scale)
+	#reconstruction_water = back_project(filtered)
+
+	#mu_w = ct_detect(p, material.coeff('Water'), n*scale, do_noise = False)
+	#mu_w = 0.137 # random value taken from the mass_attenuation excel sheet
+	mu_w = 0.206 # this value for the Xtreme scanner, since peak energy is lower
 
 	# use result to convert to hounsfield units
-	reconstruction_hu = reconstruction - reconstruction_water
-	reconstruction_hu = np.divide(reconstruction_hu, reconstruction_water)*1000
+	reconstruction_hu = reconstruction - mu_w
+	reconstruction_hu = np.divide(reconstruction_hu, mu_w)*1000
 
 	reconstruction = reconstruction_hu
 
 	# limit minimum to -1024, which is normal for CT data.
 	# setting the lower and the upper bounds for HU
 	reconstruction = np.clip(reconstruction, -1024, 3072)
+
+	"""
+	All this is done in the viewer
+	-------------------------------
 
 	# converting HU to appropriate pixel values
 	c = 0
@@ -50,5 +58,6 @@ def hu(p, material, reconstruction, scale):
 	#g = np.clip(g, 0, 255)
 
 	reconstruction = g
+	"""
 
 	return reconstruction
